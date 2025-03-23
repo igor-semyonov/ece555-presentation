1549948b0c9fe8558c153a0a149ed19d497aee74
Igor Semyonov <igor@semyonov.xyz>
2025-03-23 16:51:33 -0400

    working on mandelbrot cuda

--- code/cpu/mandelbrot/src/main.rs
+++ code/cpu/mandelbrot/src/main.rs
@@ -1,9 +1,19 @@
+use anyhow::{Context, Result, anyhow};
+use cust::function::{BlockSize, GridSize};
+use cust::prelude::*;
 use image::GrayImage;
 use ndarray as nd;
 use ndarray::parallel::prelude::*;
 use num::complex::Complex64 as c64;
+use vek::Vec2;
 
-fn main() {
+static PTX: &str =
+    include_str!("../../../resources/mandelbrot.ptx");
+
+const NROWS: usize = 32;
+const NCOLS: usize = 32;
+
+fn main() -> Result<()> {
     let zn_limit = 100;
     let x_min = -2.0;
     let x_max = 1.0;
@@ -11,53 +21,105 @@ fn main() {
     let y_max = 1.0;
     let x_range = x_max - x_min;
     let y_range = y_max - y_min;
-    let n_rows = 16384;
-    let n_cols = 8192;
 
     let points = nd::Array2::from_shape_fn(
         (
-            n_rows, n_cols,
+            NROWS, NCOLS,
         ),
         |idx| c64 {
             re: x_range
-                * (idx.0 as f64 / ((n_rows - 1) as f64))
+                * (idx.0 as f64 / ((NROWS - 1) as f64))
                 + x_min,
             im: y_range
-                * (idx.1 as f64 / ((n_cols - 1) as f64))
+                * (idx.1 as f64 / ((NCOLS - 1) as f64))
                 + y_min,
         },
     );
 
     let mut mandel: nd::Array2<u8> = nd::Array2::zeros((
-        n_rows, n_cols,
+        NROWS, NCOLS,
     ));
+    let mandel = vec![0usize; NROWS * NCOLS];
 
-    nd::Zip::from(&points)
-        .and(&mut mandel)
-        .into_par_iter()
-        .with_min_len(100)
-        .for_each(
-            |(c, m)| {
-                let mut z = *c;
-                for _ in 0..zn_limit {
-                    z = z * z + c;
-                    if z.norm() > 2.0 {
-                        *m = 0;
-                        return;
-                    }
-                }
-                *m = 255;
-            },
-        );
+    // nd::Zip::from(&points)
+    //     .and(&mut mandel)
+    //     .into_par_iter()
+    //     .with_min_len(100)
+    //     .for_each(
+    //         |(c, m)| {
+    //             let mut z = *c;
+    //             for _ in 0..zn_limit {
+    //                 z = z * z + c;
+    //                 if z.norm() > 2.0 {
+    //                     *m = 0;
+    //                     return;
+    //                 }
+    //             }
+    //             *m = 255;
+    //         },
+    //     );
 
-    let mandel = mandel
-        .t()
-        .as_standard_layout()
-        .to_owned();
-    let image = array_to_image(mandel);
-    image
-        .save("out.png")
-        .unwrap();
+    // initialize CUDA, this will pick the first available
+    // device and will make a CUDA context from it.
+    // We don't need the context for anything but it must be
+    // kept alive.
+    let _ctx = cust::quick_init()?;
+
+    // Make the CUDA module, modules just house the GPU code
+    // for the kernels we created. they can be made from
+    // PTX code, cubins, or fatbins.
+    let module = Module::from_ptx(
+        PTX,
+        &[],
+    )?;
+
+    // make a CUDA stream to issue calls to. You can think
+    // of this as an OS thread but for dispatching
+    // GPU calls.
+    let stream = Stream::new(
+        StreamFlags::NON_BLOCKING,
+        None,
+    )?;
+
+    let mandel_gpu = mandel
+        .as_slice()
+        // .unwrap()
+        .as_dbuf()?;
+
+    let threads = Vec2::broadcast(16usize);
+    let block_size: BlockSize = threads.into();
+    let grid_size: GridSize =
+        (Vec2::from_slice(&[NROWS, NCOLS]) / threads)
+            .into();
+    println!(
+        "{:?}\n{:?}\n",
+        block_size, grid_size
+    );
+
+    println!("{}", mandel_gpu.len());
+
+    unsafe {
+        launch!(
+            module.mandelbrot<<<grid_size, block_size, 0, stream>>>(
+                mandel_gpu.as_device_ptr()
+                // 0u8
+            )
+        )?;
+    }
+
+    stream.synchronize()?;
+
+    // let image = array_to_image(
+    //     mandel
+    //         .t()
+    //         .as_standard_layout()
+    //         .to_owned(),
+    // );
+    // image
+    //     .save("out.png")
+    //     .unwrap();
+
+    Ok(())
 }
 
 fn array_to_image(arr: nd::Array2<u8>) -> GrayImage {
