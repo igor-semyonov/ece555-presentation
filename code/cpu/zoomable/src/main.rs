c970f303a6c045eef74407999c661870891c3339
Igor Semyonov <igor@semyonov.xyz>
2025-03-26 23:54:42 -0400

    zoom working!

--- code/cpu/zoomable/src/main.rs
+++ code/cpu/zoomable/src/main.rs
@@ -14,7 +14,7 @@ use iyes_perf_ui::prelude::*;
 
 static PTX: &str =
     include_str!("../../../resources/mandelbrot.ptx");
-const N_RE: usize = 1 << 13;
+const N_RE: usize = 1 << 11;
 const N_IM: usize = N_RE >> 1;
 const ZN_LIMIT: u32 = 100;
 
@@ -28,11 +28,19 @@ fn main() {
         .add_plugins(bevy::diagnostic::SystemInformationDiagnosticsPlugin)
         .add_plugins(bevy::render::diagnostic::RenderDiagnosticsPlugin)
         .add_plugins(PerfUiPlugin)
-        .add_systems(Update, scroll_events)
+        .add_systems(Update, (
+                scroll_events,
+                arrow_events
+        ))
         .add_systems(Update, update_viewport)
         .add_event::<ViewportBoundsEvent>()
         .add_systems(Startup, setup)
+        .add_systems(Startup, setup_gpu)
         .run();
+}
+fn setup_gpu(world: &mut World) {
+    let gpu = GpuStuff::default();
+    world.insert_non_send_resource(gpu);
 }
 
 #[derive(Component, Debug)]
@@ -52,12 +60,10 @@ impl Default for FrameBuffer {
 }
 
 #[allow(dead_code)]
-#[derive(Component)]
+#[derive(Resource)]
 struct GpuStuff {
     _ctx: cust::context::Context,
     module: cust::module::Module,
-    stream: cust::stream::Stream,
-    frame_buffer: cust::memory::DeviceBuffer<u8>,
     block_size: cust::function::BlockSize,
     grid_size: cust::function::GridSize,
 }
@@ -72,14 +78,6 @@ impl Default for GpuStuff {
             &[],
         )
         .unwrap();
-        let stream = Stream::new(
-            StreamFlags::NON_BLOCKING,
-            None,
-        )
-        .unwrap();
-        let frame_buffer: DeviceBuffer<_> =
-            cust::memory::DeviceBuffer::zeroed(N_RE * N_IM)
-                .unwrap();
         let block_size = BlockSize {
             x: 1024,
             y: 1,
@@ -93,8 +91,6 @@ impl Default for GpuStuff {
         GpuStuff {
             _ctx,
             module,
-            stream,
-            frame_buffer,
             block_size,
             grid_size,
         }
@@ -148,19 +144,49 @@ fn setup(
     commands.spawn((
         ViewportBounds::default(),
         FrameBuffer::default(),
-        // GpuStuff::default(),
     ));
     commands.spawn(Sprite::from_image(handle.clone()));
     commands.insert_resource(MyProcGenImage(handle));
 }
 
+fn arrow_events(
+    keys: Res<ButtonInput<KeyCode>>,
+    mut ev_bounds: EventWriter<ViewportBoundsEvent>,
+    mut query: Query<(&mut ViewportBounds,)>,
+) {
+    let mut direction = (
+        0i8, 0i8,
+    );
+    if keys.pressed(KeyCode::ArrowRight) {
+        direction.0 += 1;
+    }
+    if keys.pressed(KeyCode::ArrowLeft) {
+        direction.0 -= 1;
+    }
+    if keys.pressed(KeyCode::ArrowUp) {
+        direction.1 += 1;
+    }
+    if keys.pressed(KeyCode::ArrowDown) {
+        direction.1 -= 1;
+    }
+    if direction.0 != 0 || direction.1 != 0 {
+        let (mut bounds,) = query.single_mut();
+        let re_range = bounds.re_max - bounds.re_min;
+        let im_range = bounds.im_max - bounds.im_min;
+        let re_delta = 0.005 * re_range * direction.0 as f32;
+        let im_delta = 0.005 * im_range * direction.1 as f32;
+        bounds.re_min += re_delta;
+        bounds.re_max += re_delta;
+        bounds.im_min += im_delta;
+        bounds.im_max += im_delta;
+        ev_bounds.send(ViewportBoundsEvent());
+    }
+}
+
 fn scroll_events(
     mut evr_scroll: EventReader<MouseWheel>,
     mut ev_bounds: EventWriter<ViewportBoundsEvent>,
-    mut query: Query<(
-        Entity,
-        &mut ViewportBounds,
-    )>,
+    mut query: Query<(&mut ViewportBounds,)>,
 ) {
     use bevy::input::mouse::MouseScrollUnit;
     for ev in evr_scroll.read() {
@@ -173,7 +199,7 @@ fn scroll_events(
                 // horizontal: {}",
                 //     ev.y, ev.x
                 // );
-                zoom_nudge += 0.1 * ev.y;
+                zoom_nudge -= 0.1 * ev.y;
             }
             MouseScrollUnit::Pixel => {
                 // println!(
@@ -185,22 +211,22 @@ fn scroll_events(
             }
         }
         zoom_factor += zoom_nudge;
-        let (_entity, mut bound) = query.single_mut();
-        bound.re_min *= zoom_factor;
-        bound.re_max *= zoom_factor;
-        bound.im_min *= zoom_factor;
-        bound.im_max *= zoom_factor;
+        let (mut bounds,) = query.single_mut();
+        bounds.re_min *= zoom_factor;
+        bounds.re_max *= zoom_factor;
+        bounds.im_min *= zoom_factor;
+        bounds.im_max *= zoom_factor;
         ev_bounds.send(ViewportBoundsEvent());
     }
 }
 
 fn update_viewport(
-    mut _ev_bounds: EventReader<ViewportBoundsEvent>,
+    mut ev_bounds: EventReader<ViewportBoundsEvent>,
+    gpu: NonSend<GpuStuff>,
     my_handle: Res<MyProcGenImage>,
     mut images: ResMut<Assets<Image>>,
     mut query: Query<(
         &ViewportBounds,
-        // &GpuStuff,
         &mut FrameBuffer,
     )>,
 ) {
@@ -208,79 +234,134 @@ fn update_viewport(
     //     query.single_mut();
     let (bounds, mut frame_buffer) = query.single_mut();
 
-    let re_range = bounds.re_max - bounds.re_min;
-    let im_range = bounds.im_max - bounds.im_min;
+    for _ in ev_bounds
+        .read()
+        .into_iter()
+    {
+        println!(
+            "{:?}",
+            bounds
+        );
+        let re_range = bounds.re_max - bounds.re_min;
+        let im_range = bounds.im_max - bounds.im_min;
 
-    use cust::function::{BlockSize, GridSize};
-    use cust::prelude::*;
-    let _ctx: cust::context::Context =
-        cust::quick_init().unwrap();
-    let module = Module::from_ptx(
-        PTX,
-        &[],
-    )
-    .unwrap();
-    let stream = Stream::new(
-        StreamFlags::NON_BLOCKING,
-        None,
-    )
-    .unwrap();
-    let gpu_frame_buffer: DeviceBuffer<_> =
-        cust::memory::DeviceBuffer::zeroed(N_RE * N_IM)
-            .unwrap();
-    let block_size = BlockSize {
-        x: 1024,
-        y: 1,
-        z: 1,
-    };
-    let grid_size = GridSize {
-        x: N_IM as u32 >> 10,
-        y: N_RE as u32,
-        z: 1,
-    };
-    unsafe {
-        launch!(
-                module.mandelbrot_local_points<<<
-        grid_size, block_size, 0, stream>>>(
-                    N_RE,
-                    N_IM,
-                    bounds.re_min,
-                    bounds.re_max,
-                    re_range,
-                    bounds.im_min,
-                    bounds.im_max,
-                    im_range,
-                    ZN_LIMIT,
-                    gpu_frame_buffer.as_device_ptr(),
+        use cust::prelude::*;
+        let module = &gpu.module;
+        let stream = Stream::new(
+            StreamFlags::NON_BLOCKING,
+            None,
+        )
+        .unwrap();
+        let gpu_frame_buffer: DeviceBuffer<_> =
+            cust::memory::DeviceBuffer::zeroed(N_RE * N_IM)
+                .unwrap();
+        let block_size = gpu.block_size;
+        let grid_size = gpu.grid_size;
+        unsafe {
+            launch!(
+                    module.mandelbrot_local_points<<<grid_size, block_size, 0, stream>>>(
+                        N_RE,
+                        N_IM,
+                        bounds.re_min,
+                        bounds.re_max,
+                        re_range,
+                        bounds.im_min,
+                        bounds.im_max,
+                        im_range,
+                        ZN_LIMIT,
+                        gpu_frame_buffer.as_device_ptr(),
+                    )
                 )
-            )
-        .unwrap();
-    }
+            .unwrap();
+        }
+        stream
+            .synchronize()
+            .unwrap();
+        match gpu_frame_buffer.copy_to(&mut frame_buffer.0)
+        {
+            Ok(_) => {
+                let image = images
+                    .get_mut(&my_handle.0)
+                    .expect("Image not found");
+                // let new_image = Image::new(
+                //     Extent3d {
+                //         width: N_RE as u32,
+                //         height: N_IM as u32,
+                //         depth_or_array_layers: 1,
+                //     },
+                //     TextureDimension::D2,
+                //     frame_buffer.0.clone(),
+                //     TextureFormat::R8Unorm,
+                //     RenderAssetUsages::MAIN_WORLD
+                //         | RenderAssetUsages::RENDER_WORLD,
+                // );
 
-    stream
-        .synchronize()
-        .unwrap();
-    match gpu_frame_buffer.copy_to(&mut frame_buffer.0) {
-        Ok(_) => {
-            let image = images
-                .get_mut(&my_handle.0)
-                .expect("Image not found");
-            // eprintln!(
-            //     "Successfully copied device to host buffer.",
-            // );
-            image
-                .data
-                .copy_from_slice(
-                    frame_buffer
-                        .0
-                        .as_slice(),
+                // image.set(Box::new(new_image)).unwrap();
+
+                use ndarray as nd;
+                let frame_buffer_nd =
+                    nd::Array2::from_shape_vec(
+                        (
+                            N_RE, N_IM,
+                        ),
+                        frame_buffer
+                            .0
+                            .clone(),
+                    )
+                    .unwrap();
+                image
+                    .data
+                    .copy_from_slice(
+                        frame_buffer_nd
+                            .t()
+                            .as_standard_layout()
+                            .as_slice()
+                            .unwrap()
+                        // frame_buffer
+                        //     .0
+                        //     .as_slice(),
+                    );
+
+                // use ndarray as nd;
+                // let frame_buffer_nd =
+                //     nd::Array2::from_shape_vec(
+                //         (
+                //             N_RE, N_IM,
+                //         ),
+                //         frame_buffer.0.clone(),
+                //     )
+                //     .unwrap();
+                // let image_not_bevy = array_to_image(
+                //     frame_buffer_nd
+                //         .t()
+                //         // out.t()
+                //         .as_standard_layout()
+                //         .to_owned(),
+                // );
+                // image_not_bevy
+                //     .save("out.png")
+                //     .unwrap();
+            }
+            Err(x) => {
+                eprintln!(
+                    "Failed to copy device to host buffer. {}",
+                    x
                 );
-        }
-        Err(x) => {
-            eprintln!(
-                "Failed to copy device to host buffer. {}",
-                x
-            );
+            }
         }
     }
+}
+use image::GrayImage;
+use ndarray as nd;
+#[allow(dead_code)]
+fn array_to_image(arr: nd::Array2<u8>) -> GrayImage {
+    assert!(arr.is_standard_layout());
+
+    let (height, width) = arr.dim();
+    let raw = arr
+        .into_raw_vec_and_offset()
+        .0;
+
+    GrayImage::from_raw(width as u32, height as u32, raw)
+        .expect("container should have the right size for the image dimensions")
 }
